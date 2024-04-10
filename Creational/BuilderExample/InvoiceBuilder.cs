3d2edc683b21d127580955a4a267dfcca644dc09
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-04-10 19:13:51 +0200

    Creational - Builder

---
+++ Creational/BuilderExample/InvoiceBuilder.cs
@@ -0,0 +1,40 @@
+﻿namespace DesignPatternsNET.Creational.BuilderExample;
+
+public class InvoiceBuilder
+{
+    private Invoice _invoice = new ();
+
+    
+    public InvoiceBuilder setVendor(string vendor)
+    {
+        _invoice.Vendor = vendor;
+        return this;
+    }
+    public InvoiceBuilder setVendee(string vendee) 
+    {
+        _invoice.Vendee = vendee;
+        return this;
+    }
+    public InvoiceBuilder setNumber(string number) 
+    {
+        _invoice.Number = number;
+        return this;
+    }
+    public InvoiceBuilder setNote(string note) 
+    {
+        _invoice.Note = note;
+        return this;
+    }
+    public InvoiceBuilder setDate(DateTime date) 
+    {
+        _invoice.Date = date;
+        return this;
+    }
+    public InvoiceBuilder setLineItems(IEnumerable<string> lineItems) 
+    {
+        _invoice.LineItems = lineItems;
+        return this;
+    }
+    
+    public Invoice Build() => _invoice;
+}
