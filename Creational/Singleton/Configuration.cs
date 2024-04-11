54b02b5c4f8c434f5be3ec687ce9aee2aff96db3
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-04-11 14:03:39 +0200

    Creational - Singleton

---
+++ Creational/Singleton/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatternsNET.Creational.Singleton;
+
+public class Configuration
+{
+    private static Configuration _instance = null;
+    private static object obj = new ();
+    public string StringProperty { get; set; }
+    public int IntProperty { get; set; }
+    
+    private Configuration() {}
+
+    public static Configuration Instance()
+    {
+        lock (obj)
+        {
+            if (_instance == null) _instance = new Configuration();
+        }
+
+        return _instance;
+    }
+}
---
+++ Creational/Singleton/SingletonApp.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatternsNET.Creational.Singleton;
+
+public static class SingletonApp
+{
+    public static void Run()
+    {
+        var configuration1 = Configuration.Instance();
+        configuration1.StringProperty = "Hello";
+        configuration1.IntProperty = 42;
+        Console.WriteLine($"String property for configuration1: {configuration1.StringProperty}");
+        Console.WriteLine($"Int property for configuration1: {configuration1.IntProperty}");
+        
+        var configuration2 = Configuration.Instance();
+        
+        Console.WriteLine($"Reference equality: {ReferenceEquals(configuration1, configuration2)}");
+        Console.WriteLine(ReferenceEquals(configuration1, configuration2) ? "Singleton works" : "Singleton failed");
+        Console.WriteLine($"String property for configuration2: {configuration2.StringProperty}");
+        Console.WriteLine($"Int property for configuration2: {configuration2.IntProperty}");
+    }
+}
